9f755f556bcaacf796cf7389ed996c7d6e29b5aa
Marcelo Elizeche Landó <melizeche@gmail.com>
2017-08-22 07:25:27 -0400

    better setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,22 @@ setup(
     name='dolarpy',
     packages=['dolarpy'],
     version='0.1.3',
-    description=('Wrapp8er for DolarPy, an PYG/USD exchange rate API.'),
+    description=('Wrapper for DolarPy, an PYG/USD exchange rate API.'),
     author='Marcelo Elizeche Landó',
     author_email='melizeche@gmail.com',
     url='https://github.com/melizeche/dolarpy-wrapper-python',
     license='MIT',
     use_2to3=True,
-    install_requires=required
+    install_requires=required,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers'
+        'Intended Audience :: Financial and Insurance Industry''
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial'
+    ]
 )
